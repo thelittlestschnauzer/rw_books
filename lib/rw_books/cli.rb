32916c4def2db9156368f80d67ec5e0936a8b864
the littlest schnauzer <thelittlestschnauzer@gmail.com>
2017-10-17 11:06:08 -0400

    method list_books which will end up scraping the books but for now is just listing the books

--- lib/rw_books/cli.rb
+++ lib/rw_books/cli.rb
@@ -2,8 +2,19 @@ class RwBooks::CLI #this is charge of user interation
 
   def call
     puts "Here are the books Reese Witherspoon Book Club is reading this month:"
-    #list_books
+    list_books
+      #this method should list the four books the group is working on within the four week period
   end
+
+  def list_books
+    #this method will list the four books the group is reading
+    puts <<-DOC
+      1. Slightly South of Simple
+      2. The Life She was Given
+      3. A Stranger in the House
+      4. The Rules of Magic
+      DOC
+    end
 
 
 end
