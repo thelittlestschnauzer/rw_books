24ac6e05d81c75b65657676693966789afb4bff0
the littlest schnauzer <thelittlestschnauzer@gmail.com>
2017-10-23 12:39:53 -0400

    still working on method list_books on how to set up to scrape

--- lib/rw_books/books.rb
+++ lib/rw_books/books.rb
@@ -3,18 +3,12 @@ class RwBooks::Books
   attr_accessor :title, :author, :url
 
   def self.this_month
-    doc = Nokogiri::HTML(open(https://www.goodreads.com/group/show/209532-rw-bookclub))
-
-    books = {}
-
-    doc.css("div.Title a.bookTitle").each do |book|
-      books = {
-        :books => doc.css("a.bookName").text
-      }
-    end
-    books
+    self.scrape_books
   end
 
+  def self.scrape_books
+
+  end
 
 
 end
--- lib/rw_books/cli.rb
+++ lib/rw_books/cli.rb
@@ -6,7 +6,11 @@ class RwBooks::CLI #this is charge of user interation
 
     def list_books
       puts "Welcome to the Reese Witherspoon Book Club! Here is a list of the books we are currently reading:"
-      
+      #going to want this to scrape the books from the website and list here hoping the below code will be correct but not sure yet
+      # @books = RwBooks::Books.this_month
+      # @books.each.with_index do |book, i|
+      #   puts "#{i}. #{"book.title"} - by #{book.author}"
+      # end
     end
 
 end
